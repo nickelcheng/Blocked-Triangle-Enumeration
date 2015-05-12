2782111ba38d978d6834a96bc60cc97893774ebd
Nickel Cheng <nickel.y.j.cheng@gmail.com>
2015-05-12 07:09:58 -0400

    add count time function for brute force

--- Methods/BruteForce/bruteForce.cpp
+++ Methods/BruteForce/bruteForce.cpp
@@ -1,40 +1,43 @@
 #include<cstdio>
 #include<cstdlib>
-
-bool edge[10000][10000];
+#include "timer.h"
 
 int main(int argc, char *argv[]){
     if(argc != 3){
-        fprintf(stderr, "usage: bruteForce <input_path> < node_num>\n");
+        fprintf(stderr, "usage: bruteForce <input_path> <node_num>\n");
         return 0;
     }
 
     int nodeNum = atoi(argv[2]);
 
     FILE *fp = fopen(argv[1], "r");
-    if(nodeNum > 10000){
-        fprintf(stderr, "too many nodes\n");
-        return 0;
-    }
 
     int u, v;
+    bool *edge = (bool*)malloc(nodeNum*nodeNum*sizeof(bool));
     while(fscanf(fp, "%d%d", &u, &v) != EOF){
-        edge[u][v] = edge[v][u] = true;
+        edge[u*nodeNum+v] = edge[v*nodeNum+u] = true;
     }
     fclose(fp);
+
+    timerInit(1)
+    timerStart(0)
     int triNum = 0;
     for(int i = 0; i < nodeNum; i++){
         for(int j = i+1; j < nodeNum; j++){
-            if(!edge[i][j]) continue;
+            if(!edge[i*nodeNum+j]) continue;
             for(int k = j+1; k < nodeNum; k++){
-                if(edge[j][k] && edge[k][i]){
+                if(edge[j*nodeNum+k] && edge[i*nodeNum+k]){
                     triNum++;
-                    printf("%d %d %d\n", i, j, k);
+//                    printf("%d %d %d\n", i, j, k);
                 }
             }
         }
     }
-    fprintf(stderr, "total triangle: %d\n", triNum);
+    timerEnd("total", 0)
+
+    free(edge);
+
+    printf("total triangle: %d\n", triNum);
 
     return 0;
 }
