3adbcdc2627ecf9cb1a4591c8d6eb5e4b9928bca
Nickel Cheng <nickel.y.j.cheng@gmail.com>
2015-05-20 00:38:22 +0800

    add listArray

---
+++ Algorithm/header/listArray.h
@@ -0,0 +1,25 @@
+#ifndef __LIST_ARRAY_H__
+#define __LIST_ARRAY_H__
+//#ifdef __CUDACC__
+#define DECORATE __host__ __device__
+/*#else
+#define DECORATE
+#endif*/
+#include "struct.h"
+#include <cuda_runtime.h>
+
+class ListArray{
+    public:
+        ~ListArray(); // destructor
+        void initArray(vector< Edge > edge, int n);
+        int getMaxDegree() const;
+        DECORATE int getDeg(int v) const;
+        DECORATE int getNodeNum() const;
+        DECORATE const int* neiStart(int v) const;
+        DECORATE void print() const;
+//    private:
+        int *nodeArr, nodeNum;
+        int *edgeArr, edgeNum;
+};
+
+#endif
---
+++ Algorithm/src/listArray.cu
@@ -0,0 +1,67 @@
+#include "listArray.h"
+#include<algorithm>
+#include<cstring>
+#include<cstdio>
+
+ListArray::~ListArray(void){
+    delete [] nodeArr;
+    delete [] edgeArr;
+}
+
+void ListArray::initArray(vector< Edge > edge, int n){
+    nodeNum = n, edgeNum = (int)edge.size();
+    nodeArr = new int[sizeof(int)*(n+1)];
+    edgeArr = new int[sizeof(int)*edgeNum];
+    memset(nodeArr, -1, sizeof(int)*(nodeNum+1));
+
+    std::sort(edge.begin(), edge.end());
+
+    edge.push_back(Edge(nodeNum, -1));
+    for(int i = 0; i < edgeNum; i++){
+        edgeArr[i] = edge[i].v;
+        if(edge[i].u != edge[i+1].u){
+            nodeArr[edge[i+1].u] = i+1;
+        }
+    }
+    edge.pop_back();
+
+    nodeArr[edge[0].u] = 0;
+    for(int i = nodeNum; i > 0; i--){
+        if(nodeArr[i-1] == -1) nodeArr[i-1] = nodeArr[i];
+    }
+}
+
+int ListArray::getMaxDegree(void) const{
+    int mmax = 0;
+    for(int i = 0; i < nodeNum; i++){
+        int deg = nodeArr[i+1] - nodeArr[i];
+        if(deg > mmax) mmax = deg;
+    }
+    return mmax;
+}
+
+__host__ __device__ int ListArray::getDeg(int v) const{
+    if(v < 0 || v >= nodeNum) return 0;
+    return nodeArr[v+1] - nodeArr[v];
+}
+
+DECORATE int ListArray::getNodeNum() const{
+    return nodeNum;
+}
+
+DECORATE const int* ListArray::neiStart(int v) const{
+    if(v < 0 || v >= nodeNum) return 0;
+    return &edgeArr[nodeArr[v]];
+}
+
+DECORATE void ListArray::print(void) const{
+    printf("node:");
+    for(int i = 0; i <= nodeNum; i++){
+        printf(" %d", nodeArr[i]);
+    }
+    printf("\nedge:");
+    for(int i = 0; i < edgeNum; i++){
+        printf(" %d", edgeArr[i]);
+    }
+    printf("\n");
+}
